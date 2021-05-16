c8f0624c742ef01af8eaf7276ba8feb5071026fa
afes.okta <afes.okta@gmail.com>
2021-05-16 21:50:56 +0700

    add form vue ktp-reader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>  
+    <v-app-bar app color="primary" dark>  
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title to="/">{{ appName }}</v-toolbar-title>          
+    </v-app-bar>
+        
+    <v-content>
+      <v-container fluid>
+        <FormKtp />
+      </v-container>
+    </v-content>
+
+    <v-card>
+      <v-footer absolute app>
+        <v-card-text class="text-center">
+          &copy; {{ new Date().getFullYear() }} — <strong>KIOSK</strong>
+        </v-card-text>
+      </v-footer>
+    </v-card>
+  
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+  //  Header: () => import( /* webpackChunckName: "header" */ '@/components/layouts/Header.vue'),
+   // HelloWorld: () => import( /* webpackChunckName: "helloworld" */ '@/components/HelloWorld.vue'),    
+    FormKtp: () => import( /* webpackChunckName: "formktp" */ '@/components/form-ktp/FormKtp.vue')    
+    // Alert: () => import( /* webpackChunkName: "alert" */ '@/components/Alert.vue'),
+    // Search: () => import( /* webpackChunkName: "search" */ '@/components/Search.vue'),
+    // Login: () => import( /* webpackChunkName: "login" */ '@/components/Login.vue'),
+    // Register: () => import( /* webpackChunkName: "register" */ '@/components/Register.vue'),
+    // Cart: () => import( /* webpackChunkName: "cart" */ '@/components/Cart.vue'),    
+  },
+    
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/form-ktp/FormKtp.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h2>Silahkan Input / Scan KTP Anda</h2>
+        </v-card-title>
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="idktp" :counter="16" :rules="idRules" label="No. KTP" required></v-text-field>
+            </v-col>                
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="name"  :rules="nameRules" label="Nama" required></v-text-field>
+            </v-col>
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="pekerjaan"  label="Pekerjaan" required></v-text-field>            
+            </v-col>
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="alamat"  label="Alamat" required></v-text-field>
+            </v-col>
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="desa"  label="Desa" required></v-text-field>
+            </v-col>
+            <v-col class="col-11 ml-5 mr-5">
+                <v-text-field v-model="kota"  label="Kota" required></v-text-field>    
+            </v-col>            
+            <v-col class="col-11 ml-5 mr-5">
+                <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+                    Validate
+                </v-btn>
+
+                <v-btn color="error" class="mr-4" @click="reset">
+                Reset Form
+                </v-btn>
+            </v-col>
+        </v-form>
+    </v-card>
+</template>
+<script>
+export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 50) || 'Name must be less than 50 characters',
+      ],
+      idktp: '',
+      idRules: [
+        v => !!v || 'NIK is required',
+        v => (v && v.length <= 16) || 'NIK must be less than 16 characters',
+      ],
+      pekerjaan: '',
+      alamat: '',
+      desa: '',
+      kota: ''
+    }),
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
